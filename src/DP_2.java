78db6a3a5aad5e8704be093cace04cc0fdc9f759
taehyun <taehyun16@naver.com>
2021-02-02 14:15:20 +0900

    Initial commit

---
+++ src/DP_2.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class DP_2 {
+    public static int solution(int[][] triangle) {
+        int answer = 0;
+
+        for (int i = 1; i < triangle.length; i++) {
+            triangle[i][0] += triangle[i - 1][0];
+            triangle[i][i] += triangle[i - 1][i - 1];
+            for (int j = 1; j < i; j++)
+                triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+        }
+
+        answer = Arrays.stream(triangle[triangle.length - 1]).max().getAsInt();
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(DP_2.solution(new int[][]{{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}}));
+    }
+}
---
+++ src/binary_search/Immigration.java
@@ -0,0 +1,5 @@
+package binary_search;
+
+public class Immigration {
+
+}
