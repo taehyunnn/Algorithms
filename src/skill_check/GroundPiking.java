37462901ec299f7e58f607f7cd9bbb2e0372dc0c
taehyun <taehyun16@naver.com>
2021-02-17 18:34:44 +0900

    실력 체크 레벨 2 - 땅따먹기, 순위검색

---
+++ src/skill_check/GroundPiking.java
@@ -0,0 +1,25 @@
+package skill_check;
+
+public class GroundPiking {
+    int solution(int[][] land) {
+        int answer = 0;
+
+        int[][] dp = new int[land.length][4];
+
+        for (int i = 0; i < 4; i++) {
+            dp[0][i] = land[0][i];
+        }
+
+        for (int i = 1; i < land.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                dp[i][j] = Math.max( Math.max(dp[i-1][(j+1)%4], dp[i-1][(j+2)%4]), dp[i-1][(j+3)%4]) + land[i][j];
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            answer = Math.max(answer, dp[dp.length-1][i]);
+        }
+
+        return answer;
+    }
+}
