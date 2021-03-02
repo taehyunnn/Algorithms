af2560661161be79add063f4648374e23264f322
taehyun <taehyun16@naver.com>
2021-03-02 20:00:15 +0900

    연습문제 - 줄 서는 방법
    int 와 long 자료형을 잘 보자 ㅜㅜ...

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 
-public class Main {
+class Main {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        long temp = k - 1;
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+        for (int i = 0; i < n; i++) {
+            long num = temp / factorial(n - i - 1);
+            answer[i] = list.get((int)num);
+            list.remove((int)num);
+            temp = temp % factorial(n - i - 1);
+        }
 
-}
+        return answer;
+    }
 
-class Sub extends Main {
-
+    public long factorial(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
