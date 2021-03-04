ac86a149ef45b881ed99b4db7e7463aaa101d0a8
taehyun <taehyun16@naver.com>
2021-03-04 20:53:50 +0900

    2020 카카오 - 외벽 점검
    원형으로 주어진 완전탐색 문제임을 파악하자.....
    permutation 을 활용하여 경우의 수를 모두 체크

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,77 @@
 import java.util.ArrayList;
-
 class Main {
-    public int[] solution(int n, long k) {
-        int[] answer = new int[n];
-        long temp = k - 1;
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            list.add(i);
+    int min = Integer.MAX_VALUE;
+    int[] weak;
+    int[] permutationTemp;
+
+    public int solution(int n, int[] weak, int[] dist) {
+        int answer = -1;
+
+        this.weak = weak;
+        permutationTemp = new int[dist.length];
+
+        // 배열 shift
+        for (int i = 0; i < weak.length; i++) {
+            permute(0, dist.length, dist, new boolean[dist.length]);
+            shift(weak, n);
         }
-        for (int i = 0; i < n; i++) {
-            long num = temp / factorial(n - i - 1);
-            answer[i] = list.get((int)num);
-            list.remove((int)num);
-            temp = temp % factorial(n - i - 1);
+
+        if (min != Integer.MAX_VALUE) {
+            answer = min;
         }
 
         return answer;
     }
 
-    public long factorial(int n) {
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
+    private void calc(int[] dist) {
+        int checkWeak = 0;
+        int currentUseCnt = 0;
+        int index = 0;
+
+        // 섞인 친구 순서대로 계산
+        for (int j = 0; j < dist.length; j++) {
+            int current = 0;
+
+            for (int k = index; k < weak.length; k++) {
+                if (weak[index] + dist[j] >= weak[k]) {
+                    current++;
+                } else {
+                    index = k;
+                    break;
+                }
+            }
+            checkWeak += current;
+            if (checkWeak == weak.length) {
+                currentUseCnt = j + 1;
+                break;
+            }
         }
-        return result;
+
+        if (checkWeak == weak.length) {
+            min = Math.min(min, currentUseCnt);
+        }
+    }
+
+    private void permute(int current, int target, int[] dist, boolean[] visit) {
+        if (current == target) {
+            calc(permutationTemp);
+            return;
+        }
+        for (int i = 0; i < dist.length; i++) {
+            if (visit[i]) {
+                continue;
+            }
+
+            visit[i] = true;
+            permutationTemp[current] = dist[i];
+            permute(current + 1, target, dist, visit);
+            visit[i] = false;
+        }
+    }
+
+    private void shift(int[] weak, int n) {
+        int temp = weak[0];
+        System.arraycopy(weak, 1, weak, 0, weak.length - 1);
+        weak[weak.length - 1] = temp + n;
     }
 }
