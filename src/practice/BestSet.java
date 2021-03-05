28a0ea1ea0ba1c5b97153dbf325da89bf55ab013
taehyun <taehyun16@naver.com>
2021-03-05 20:19:20 +0900

    연습 문제 - 최고의 집합

---
+++ src/practice/BestSet.java
@@ -0,0 +1,24 @@
+package practice;
+
+import java.util.Arrays;
+
+public class BestSet {
+    public int[] solution(int n, int s) {
+        int[] answer = new int[n];
+
+        if (n > s) {
+            return new int[]{-1};
+        }
+
+        int temp = s/n;
+
+        Arrays.fill(answer, temp);
+        int gap = s - temp * n;
+
+        for (int i = 0; i < gap; i++) {
+            answer[answer.length - 1 - i] ++;
+        }
+
+        return answer;
+    }
+}
