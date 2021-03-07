a49fb5e6b3a3c24159f75a0f39e7ba669cad0a18
taehyun <taehyun16@naver.com>
2021-03-07 17:37:29 +0900

    카카오 2019 - 길 찾기 게임
    루트 노드부터 검사해서 노드가 들어갈 적절할 위치를 찾는 문제

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,29 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+
 class Main {
-    int min = Integer.MAX_VALUE;
-    int[] weak;
-    int[] permutationTemp;
+    public int solution(int[] stones, int k) {
+        int answer = Integer.MAX_VALUE;
 
-    public int solution(int n, int[] weak, int[] dist) {
-        int answer = -1;
 
-        this.weak = weak;
-        permutationTemp = new int[dist.length];
+        for (int i = 0; i <= stones.length - k; i++) {
+            int temp = i;
+            int stone = stones[i];
+            for (int j = i; j < i + k; j++) {
+                if (stones[j] > stone) {
+                    stone = stones[j];
+                    temp = j;
+                }
+            }
+            if (answer > stone) {
+                answer = stone;
+            }
+            i = temp;
 
-        // 배열 shift
-        for (int i = 0; i < weak.length; i++) {
-            permute(0, dist.length, dist, new boolean[dist.length]);
-            shift(weak, n);
         }
-
-        if (min != Integer.MAX_VALUE) {
-            answer = min;
-        }
-
         return answer;
     }
 
-    private void calc(int[] dist) {
-        int checkWeak = 0;
-        int currentUseCnt = 0;
-        int index = 0;
-
-        // 섞인 친구 순서대로 계산
-        for (int j = 0; j < dist.length; j++) {
-            int current = 0;
-
-            for (int k = index; k < weak.length; k++) {
-                if (weak[index] + dist[j] >= weak[k]) {
-                    current++;
-                } else {
-                    index = k;
-                    break;
-                }
-            }
-            checkWeak += current;
-            if (checkWeak == weak.length) {
-                currentUseCnt = j + 1;
-                break;
-            }
-        }
-
-        if (checkWeak == weak.length) {
-            min = Math.min(min, currentUseCnt);
-        }
-    }
-
-    private void permute(int current, int target, int[] dist, boolean[] visit) {
-        if (current == target) {
-            calc(permutationTemp);
-            return;
-        }
-        for (int i = 0; i < dist.length; i++) {
-            if (visit[i]) {
-                continue;
-            }
-
-            visit[i] = true;
-            permutationTemp[current] = dist[i];
-            permute(current + 1, target, dist, visit);
-            visit[i] = false;
-        }
-    }
-
-    private void shift(int[] weak, int n) {
-        int temp = weak[0];
-        System.arraycopy(weak, 1, weak, 0, weak.length - 1);
-        weak[weak.length - 1] = temp + n;
+    public static void main(String[] args) {
+        System.out.println(new Main().solution(new int[]{2}, 2));
     }
 }
