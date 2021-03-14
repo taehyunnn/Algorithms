85e727d0cc386317112236528f527a5a2bf54bd1
taehyun <taehyun16@naver.com>
2021-03-15 03:00:20 +0900

    백준 - 합리적인 이동경로
    다익스트라를 써도 되는 문제인데 생각하지 못하고 플로이드 워셜로 풀었다..
    
    모든 점에서 거리의 최소값은 필요 없고 도착지점인 2를 기준으로 다른 정점까지의 최소값만 있으면 되므로 다익스트라 문제!!
    
    우선순위 큐를 이용해서 dist 배열을 세팅해주고, dfs를 이용해서 dp 배열을 채워주면 끝

--- src/baekjoon/ReasonablePath_2176.java
+++ src/baekjoon/ReasonablePath_2176.java
@@ -7,11 +7,12 @@ import java.util.*;
 
 public class ReasonablePath_2176 {
 
-    private static int answer = 0;
     private static final int BIG = 999999999;
     private static final int START = 1;
     private static final int END = 2;
-
+    private static List<List<int[]>> links = new ArrayList<>();
+    private static int[] dist;
+    private static int[] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
@@ -20,35 +21,44 @@ public class ReasonablePath_2176 {
         int node = Integer.parseInt(stringTokenizer.nextToken());
         int edge = Integer.parseInt(stringTokenizer.nextToken());
 
-        List<List<int[]>> links = new ArrayList<>();
         for (int i = 0; i <= node; i++) {
             links.add(new ArrayList<>());
         }
 
-        int[] dist = new int[node + 1];
+        dist = new int[node + 1];
         Arrays.fill(dist, BIG);
         for (int i = 0; i < edge; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             int a = Integer.parseInt(stringTokenizer.nextToken());
             int b = Integer.parseInt(stringTokenizer.nextToken());
             int cost = Integer.parseInt(stringTokenizer.nextToken());
-            links.get(a).add(new int[]{b,cost});
-            links.get(b).add(new int[]{a,cost});
+            links.get(a).add(new int[]{b, cost});
+            links.get(b).add(new int[]{a, cost});
         }
 
-        dijkstra(dist, links);
+        dijkstra();
 
-        int[] dp = new int[node + 1];
-        go(dp);
-
-        System.out.println(answer);
-    }
-
-    private static void go(int[] dp) {
+        dp = new int[node + 1];
+        Arrays.fill(dp,-1);
+        System.out.println(go(START));
 
     }
 
-    private static void dijkstra(int[] dist, List<List<int[]>> board) {
+    private static int go(int cur) {
+        if (dp[cur] != -1) return dp[cur];
+        if (cur == 2) return dp[cur] = 1;
+        dp[cur] = 0;
+        for (int[] info : links.get(cur)) {
+            int next = info[0];
+            if (dist[cur] > dist[next]) {
+                dp[cur] += go(next);
+            }
+        }
+        return dp[cur];
+    }
+
+
+    private static void dijkstra() {
         PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
         queue.add(new int[]{END, 0});
         dist[END] = 0;
@@ -57,18 +67,16 @@ public class ReasonablePath_2176 {
             int[] current = queue.poll();
 
             int currentNode = current[0];
-            int currentDist = current[1];
 
-            for (int[] ints : board.get(currentNode)) {
+            for (int[] ints : links.get(currentNode)) {
                 int nextNode = ints[0];
-                int nextNodeDist = ints[1];
+                int cost = ints[1];
 
-                if (dist[nextNode] > dist[currentNode] + nextNodeDist) {
-                    dist[nextNode] = dist[currentNode] + nextNodeDist;
+                if (dist[nextNode] > dist[currentNode] + cost) {
+                    dist[nextNode] = dist[currentNode] + cost;
                     queue.add(new int[]{nextNode, dist[nextNode]});
                 }
             }
         }
-
     }
 }
