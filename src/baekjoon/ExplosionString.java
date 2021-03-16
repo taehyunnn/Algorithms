8b9f07890c357133a6ae2890b9e5428c4d7df1d8
taehyun <taehyun16@naver.com>
2021-03-16 14:39:59 +0900

    백준 - 문자열 폭탄
    제목과 문제를 봤을 때는 문자열로 풀면 간단한 문제라고 생각했었다.
    
    그런데 바로 떠오르는 생각들로 문제를 풀면, 시간 초과, 메모리 초과에 걸린다.
    
    결국 해답이 떠오르지 않아 풀이를 보니 스택을 이용하는게 정답이다.
    
    문자열에서 문자를 하나씩 뽑아 배열에 넣고, 배열에 넣는 문자가 폭탄의 마지막 문자와 일치하면
    
    마지막에 넣은 단어들이 폭탄과 일치하는지 확인하고, 일치한다면 배열의 인덱스를 폭탄의 길이만큼 감소시켜준다.
    
    그러면, 다음 문자를 넣을때 덮어씌워지므로 폭탄을 제거한 문자만 남는다...

---
+++ src/baekjoon/ExplosionString.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ExplosionString {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader;
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String st = bufferedReader.readLine();
+        String explosion = bufferedReader.readLine();
+
+        char lastChar = explosion.charAt(explosion.length() - 1);
+        char[] chars = new char[st.length()];
+
+        int idx = 0;
+        for (int i = 0; i < st.length(); i++) {
+            char c = st.charAt(i);
+            chars[idx] = c;
+            if (isBomb(chars, idx, explosion)) {
+                idx -= explosion.length();
+            }
+            idx++;
+        }
+
+        String s = String.valueOf(chars, 0, idx);
+        System.out.println(s.length() == 0 ? "FRULA" : s);
+    }
+
+    private static boolean isBomb(char[] chars, int idx, String explosion) {
+        if (idx < explosion.length() - 1) {
+            return false;
+        }
+
+        for (int i = 0; i < explosion.length(); i++) {
+            if (explosion.charAt(i) != chars[i + idx - explosion.length() + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
