d176b5b9a7a79fd0fce6ca02d175bf90f7ac2a57
taehyun <taehyun16@naver.com>
2021-03-20 20:24:22 +0900

    코딩테스트

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,36 @@
-import java.io.*;
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        String[] strings = new String[]{"5.0","2.1","3.3","4.5","1.9"};
+    private static final int[][] moves = new int[][]{{1, 0}, {0, -1}, {0, 1}};
+    private static int minCount;
 
-        Arrays.sort(strings);
-        for (String string : strings) {
-            System.out.println(string);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        int n, m;
+        String[] split = input.split(" ");
+        m = Integer.parseInt(split[0]);
+        n = Integer.parseInt(split[1]);
+        minCount = n * m;
+
+        char[][] board = new char[n][m];
+        Integer[][] visit = new Integer[n][m];
+
+        List<int[]> nodes = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                board[i][j] = s.charAt(j);
+                if (board[i][j] == 'c') {
+                    nodes.add(new int[]{i, j});
+                }
+            }
         }
     }
 }
--- src/scofe/Q5.java
+++ src/scofe/Q5.java
@@ -2,7 +2,9 @@ package scofe;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Q5 {
@@ -21,7 +23,10 @@ public class Q5 {
         minCount = n * m;
 
         char[][] board = new char[n][m];
-        Integer[][] visit = new Integer[n][m];
+        int[][] visit = new int[n][m];
+        for (int[] ints : visit) {
+            Arrays.fill(ints, n * m);
+        }
 
         List<int[]> nodes = new ArrayList<>();
 
@@ -47,7 +52,7 @@ public class Q5 {
         }
     }
 
-    private static void dfs(int row, int col, char[][] board, Integer[][] visit) {
+    private static void dfs(int row, int col, char[][] board, int[][] visit) {
         if (row + 1 == board.length) {
             minCount = Math.min(minCount, visit[row][col]);
             return;
@@ -64,12 +69,10 @@ public class Q5 {
                     moveCount++;
                 }
 
-                if (visit[nextRow][nextCol] != null && moveCount >= visit[nextRow][nextCol]) {  // (nextRow, nextCol) 을 가기 위한 좌우 움직임 횟수가 기존보다 크거나 같으면 할 필요 없다.
-                    continue;
+                if (moveCount < visit[nextRow][nextCol]) {  // (nextRow, nextCol) 을 가기 위한 좌우 움직임 횟수가 기존보다 크거나 같으면 할 필요 없다.
+                    visit[nextRow][nextCol] = moveCount;
+                    dfs(nextRow, nextCol, board, visit);
                 }
-
-                visit[nextRow][nextCol] = moveCount;
-                dfs(nextRow, nextCol, board, visit);
             }
         }
     }
