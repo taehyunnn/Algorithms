a798fa3c737e97b7c7c40cb0e0fcd7bd18c1f986
taehyun <taehyun16@naver.com>
2021-03-23 14:31:02 +0900

    백준 - 양 구출하기
    트리 구조에서 dfs 하기. 자식 노드의 결과를 부모 노드가 받아준다.

--- src/baekjoon/dfs/SheepRescue.java
+++ src/baekjoon/dfs/SheepRescue.java
@@ -3,11 +3,47 @@ package baekjoon.dfs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class SheepRescue {
+    private static int[] sheep;
+    private static int[] wolfs;
+    private static List<List<Integer>> links = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+
+        int cntIsland = Integer.parseInt(stringTokenizer.nextToken());
+
+        sheep = new int[cntIsland + 1];
+        wolfs = new int[cntIsland + 1];
+
+        for (int i = 0; i <= cntIsland; i++) {
+            links.add(new ArrayList<>());
+        }
+
+        for (int i = 2; i <= cntIsland; i++) {
+            stringTokenizer = new StringTokenizer(br.readLine());
+            if ("S".equals(stringTokenizer.nextToken())) {
+                sheep[i] = Integer.parseInt(stringTokenizer.nextToken());
+            } else {
+                wolfs[i] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+            links.get(Integer.parseInt(stringTokenizer.nextToken())).add(i);
+        }
+
+        long max = dfs(1);
+        System.out.println(max);
+    }
+
+    private static long dfs(int island) {
+        long sNum = sheep[island];
+        for (Integer next : links.get(island)) {
+            sNum += dfs(next);
+        }
+        return Math.max(sNum- wolfs[island] ,0);
     }
 }
