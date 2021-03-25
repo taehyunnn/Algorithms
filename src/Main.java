781d2a6f7dc65d2f102ef06bbe7392f154243353
taehyun <taehyun16@naver.com>
2021-03-25 15:49:33 +0900

    백준 - 달빛 여우
    다익스트라 문제
    
    늑대의 경우 속도가 변하기 때문에 dist 배열을 2차원으로 만들어주어야 한다.
    dist[0][i] : i번노드 까지 늑대가 부스트를 안 썼을 때, dist[1][i] : 늑대가 부스트를 썼을 때,
    
    또 한가지, dist[1][1] 은 0으로 초기화 하면 안된다. 1번 노드가 출발지이므로 아무 생각없이 거리를 0으로 초기화했었지만
    늑대는 사이클을 돌아 1번 노드로 되돌아 온 다음에 다른 노드로 이동할때 더 빨리 이동하는 경우가 존재한다 !!! ( 속도가 변하기 때문에 )

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,191 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.*;
 
-import java.util.StringTokenizer;
-
+// 달빛 여우
+// 다익스트라 알고리즘(음의 가중치 없음)
+// 달빛여우는 1초당 1을 간다고 가정.
 public class Main {
-    static int[] sum;
-    static int[][] dp;
-    static boolean [][]check;
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        sum = new int[N + 1];
-        dp = new int[N + 1][N + 1];
-        check = new boolean[N + 1][N + 1];
+    static List<Vertex16118>[] graph;
+    static long[] foxTime; // 해당 인덱스까지 이동하는데 걸리는 최소시간
+    static long[][] wolfTime;
 
-        int table[] = new int[N + 1];
-        for (int i = 0; i < N; i++)
-            table[i] = Integer.parseInt(bf.readLine());
-        for (int i = 1; i <= N; i++)
-            sum[i] = sum[i - 1] + table[i - 1];
-        for (int i = 0 ; i<=N;i++) {
-            for(int j = 0 ; j<=N;j++)
-                dp[i][j]=Integer.MIN_VALUE/2;
+    public static void main(String args[]) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+//        InputReader in = new InputReader(System.in);
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int m = Integer.parseInt(stringTokenizer.nextToken());
+        graph = new ArrayList[n + 1];
+        wolfTime = new long[2][n + 1];    // 0 : 해당 지점에 느리게 달려왔을 때의 최소 시간. 1 : 해당 지점에 빠르게 달려왔을 때의 최소 시간.
+        foxTime = new long[n + 1];
+
+        // 다익스트라 최소시간배열 초기화
+        Arrays.fill(foxTime, Long.MAX_VALUE);
+        Arrays.fill(wolfTime[0], Long.MAX_VALUE);
+        Arrays.fill(wolfTime[1], Long.MAX_VALUE);
+
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
         }
-        System.out.println(d_dp(N, M));
-    }
-    public static int d_dp(int x, int y) {
-        if(y==0) return 0;
 
-        if(x<0) return Integer.MIN_VALUE/2;
-        if (check[x][y]) return dp[x][y];
-        check[x][y]=true;
-        dp[x][y]= d_dp(x-1,y);
-        for (int i = x; i > 0; i--) {
-            dp[x][y] = Math.max(dp[x][y], d_dp(i - 2, y - 1) + sum[x] - sum[i - 1]);
+        // 입력 값 담기
+        for (int i = 0; i < m; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+            int cost = Integer.parseInt(stringTokenizer.nextToken()) * 2;
+
+            graph[a].add(new Vertex16118(b, cost));
+            graph[b].add(new Vertex16118(a, cost));
         }
-        return dp[x][y];
+
+        dijkstra(n);
+        dijkstraWolf(n);
+
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            if (foxTime[i] < Math.min(wolfTime[0][i], wolfTime[1][i])) {    // 늑대의 속도 별 도착 최소 시간 각각을 여우 이동 최소시간과 비교해야 함
+                count++;
+            }
+        }
+
+        System.out.println(count);
     }
 
+    // 달빛늑대 다익스트라
+    private static void dijkstraWolf(int n) {
+        Queue<Vertex16118> queue = new PriorityQueue<>();
+        queue.offer(new Vertex16118(1, 0, 0));
+        wolfTime[0][1] = 0;    // 다익스트라 시작지점 초기화(첫 시작은 무조건 빠르게 시작하므로, 시작 지점에 느리게 와서 도착했다고 가정한다.)
+
+        while (!queue.isEmpty()) {
+            Vertex16118 current = queue.poll();
+            int node = current.node;
+            long nowTime = current.dist;
+
+            if (wolfTime[current.boostUsed][node] < nowTime) {    // 현재 지점까지 걸리는 최소시간보다 현재 늑대가 해당 지점에 오기까지 걸린 시간이 크면 굳이 계산할 필요가 없다.
+                continue;
+            }
+
+            for (Vertex16118 next : graph[node]) {
+                int state = 1 - current.boostUsed;    // 0 이면 1로, 1이면 0으로 스위치 전환 가능
+                long cost = wolfTime[current.boostUsed][node] + ((current.boostUsed == 0) ? next.dist / 2 : next.dist * 2);    // 상태가 0이면 빠르게, 1이면 느리게
+                if (wolfTime[state][next.node] > cost) {
+                    wolfTime[state][next.node] = cost;
+                    queue.offer(new Vertex16118(next.node, cost, state));
+                }
+            }
+        }
+    }
+
+    // 달빛여우 다익스트라
+    private static void dijkstra(int n) {
+        Queue<Vertex16118> queue = new PriorityQueue<Vertex16118>();
+        queue.offer(new Vertex16118(1, 0));
+        foxTime[1] = 0;    // 다익스트라 시작지점 초기화
+
+        while (!queue.isEmpty()) {
+            Vertex16118 end = queue.poll();
+
+            if (foxTime[end.node] < end.dist) {    // 지금 꺼낸 여우가 endIndex까지 걸린 시간이 내가 기록한 최소 시간과 다르면(더 크면) 굳이 체크할 의미가 없으니까.
+                continue;
+            }
+            for (Vertex16118 v : graph[end.node]) {
+                long cost = foxTime[end.node] + v.dist;
+                if (foxTime[v.node] > cost) {
+                    foxTime[v.node] = cost;
+                    queue.offer(new Vertex16118(v.node, cost));
+                }
+            }
+        }
+    }
+
+    // INPUT 속도 증가
+    private static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+        private SpaceCharFilter filter;
+
+        public InputReader(InputStream stream) {
+            this.stream = stream;
+        }
+
+        public int read() {
+            if (numChars == -1) {
+                throw new InputMismatchException();
+            }
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0) {
+                    return -1;
+                }
+            }
+            return buf[curChar++];
+        }
+
+        public int readInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        public boolean isSpaceChar(int c) {
+            if (filter != null) {
+                return filter.isSpaceChar(c);
+            }
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+
+        public interface SpaceCharFilter {
+            public boolean isSpaceChar(int ch);
+        }
+    }
+}
+
+class Vertex16118 implements Comparable<Vertex16118> {
+    int node;
+    long dist;
+    int boostUsed;
+
+    Vertex16118(int endIndex, long time) {
+        this.node = endIndex;
+        this.dist = time;
+    }
+
+    Vertex16118(int endIndex, long time, int state) {
+        this.node = endIndex;
+        this.dist = time;
+        this.boostUsed = state;
+    }
+
+    @Override
+    public int compareTo(Vertex16118 o) {
+        return (int)(dist - o.dist);
+    }
 }
---
+++ src/baekjoon/topologicalSort/MoonlightFox.java
@@ -0,0 +1,153 @@
+package baekjoon.topologicalSort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class MoonlightFox {
+
+    private static int n, m;
+    private static List<List<int[]>> links;
+    private static int[] fox;
+    private static int[][] wolf;
+    private static final int BIG_NUMBER = Integer.MAX_VALUE;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        n = Integer.parseInt(stringTokenizer.nextToken());
+        m = Integer.parseInt(stringTokenizer.nextToken());
+
+        links = new ArrayList<>(n + 1);
+        for (int i = 0; i <= n; i++) {
+            links.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < m; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+            int cost = Integer.parseInt(stringTokenizer.nextToken());
+
+            links.get(a).add(new int[]{b, 2 * cost});   // wolf 의 경우 *2와 /2를 해주기 때문에 int형으로 계산하기 위해 2를 미리 곱해둔다
+            links.get(b).add(new int[]{a, 2 * cost});
+        }
+
+        // 여우가 2~n번 노드까지 도착하는 최소 시간 찾고
+        dijkstraFox();
+
+        // 늑대가 2~n번 노드까지 도착하는 최소 시간 찾고
+        dijkstraWolf();
+
+        // 여우가 늑대보다 빨리 도착하는 노드의 수 계산
+        int cnt = 0;
+        for (int i = 2; i <= n; i++) {
+            if (fox[i] < wolf[0][i] && fox[i] < wolf[1][i]) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    private static void dijkstraWolf() {   // true면 부스트 사용한 상태이므로 1/2 속도, false면 부스트 사용할 수 있으므로 2배의 속도
+        wolf = new int[2][n + 1];   // wolf[0] 은 부스트 미사용, wolf[1]은 부스트 사용
+        for (int[] ints : wolf) {
+            Arrays.fill(ints, BIG_NUMBER);
+        }
+
+        Queue<Wolf> queue = new PriorityQueue<>();
+        queue.add(new Wolf(1, 0, 0));
+//        wolf[0][1] = wolf[1][1] = 0;  // 처음 문제 풀때는 아무 생각없이 1번 노드를 0으로 초기화 했지만, 늑대는 한바퀴 돌아서 1번 노드로 온 뒤에 움직이는게 더 빠를때가 있다..
+        wolf[0][1] = 0;
+
+        while (!queue.isEmpty()) {
+            Wolf current = queue.poll();
+            int currentNode = current.node;
+
+            if (wolf[current.boostUsed][currentNode] < current.dist) {
+                continue;
+            }
+
+            for (int[] next : links.get(currentNode)) {
+                int nextNode = next[0];
+                int distFromCurrentToNext = next[1];
+
+                if (current.boostUsed == 0) {
+                    distFromCurrentToNext /= 2;
+                } else {
+                    distFromCurrentToNext *= 2;
+                }
+
+                int nextNodeBoostUsed = 1-current.boostUsed;
+
+                if (wolf[nextNodeBoostUsed][nextNode] > wolf[current.boostUsed][currentNode] + distFromCurrentToNext) {
+                    wolf[nextNodeBoostUsed][nextNode] = wolf[current.boostUsed][currentNode] + distFromCurrentToNext;
+                    queue.add(new Wolf(nextNode, wolf[nextNodeBoostUsed][nextNode], nextNodeBoostUsed));
+                }
+            }
+        }
+    }
+
+    private static void dijkstraFox() {
+        fox = new int[n + 1];
+
+        Arrays.fill(fox, BIG_NUMBER);
+
+        Queue<Fox> queue = new PriorityQueue<>();
+        queue.add(new Fox(1,0));
+        fox[1] = 0;
+
+        while (!queue.isEmpty()) {
+            Fox current = queue.poll();
+            int currentNode = current.node;
+
+            if (fox[currentNode] < current.dist) {
+                continue;
+            }
+
+            for (int[] next : links.get(currentNode)) {
+                int nextNode = next[0];
+                int distFromCurrentToNext = next[1];
+
+                if (fox[nextNode] > fox[currentNode] + distFromCurrentToNext) {
+                    fox[nextNode] = fox[currentNode] + distFromCurrentToNext;
+                    queue.add(new Fox(nextNode, fox[nextNode]));
+                }
+            }
+        }
+    }
+
+    static class Fox implements Comparable<Fox>{
+        int node;
+        int dist;
+
+        public Fox(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+
+        @Override
+        public int compareTo(Fox o) {
+            return dist - o.dist;
+        }
+    }
+
+    static class Wolf implements Comparable<Wolf>{
+        int node;
+        int dist;
+        int boostUsed;  // 1이면 부스트 사용. 0이면 부스트 미사용
+
+        Wolf(int node, int dist, int boostUsed) {
+            this.node = node;
+            this.dist = dist;
+            this.boostUsed = boostUsed;
+        }
+
+        @Override
+        public int compareTo(Wolf o) {
+            return this.dist - o.dist;
+        }
+    }
+}
