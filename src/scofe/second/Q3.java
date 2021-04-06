e14d521109180f851922151adc8bf39bca789322
taehyun <taehyun16@naver.com>
2021-04-06 11:17:30 +0900

    스코페 2차 - 3번
    사이클이 없는 그래프에서 루트 노드로부터 부모 자식간의 관계를 확인하는 문제.
    
    이때 노드간에 위치를 파악하는 방법은 기준숫자(index)를 새로 두어 0부터 시작합니다. 루트부터
    깊이우선탐색을 진행하면서, 노드에 진입할 때 기준숫자를 증가하여 저장하고, 노드에서
    나갈 때 기준숫자를 증가하여 저장합니다.각 노드간에 진입할 때 숫자와 나갈 때의 숫자를 알고 있으면, 두 노드간의 부모 자식 관계를
    쉽게 파악할 수 있습니다.  부모진입(1) -> 자식진입(2) -> 자식이탈(3) -> 부모이탈(4)
    
    따라서, 부모 진입 < 자식 진입 && 부모 이탈 > 자식 이탈 이게 참이면 부모 자식 관계이다.

--- src/scofe/second/Q3.java
+++ src/scofe/second/Q3.java
@@ -6,8 +6,9 @@ import java.util.*;
 
 public class Q3 {
 
-    private static List<List<Integer>> downLinks;
-    private static List<List<Integer>> upLinks;
+    private static List<List<Integer>> links;
+    private static int[] entry;
+    private static int[] breakaway;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,12 +17,12 @@ public class Q3 {
         int n = Integer.parseInt(stringTokenizer.nextToken());
         int questionCnt = Integer.parseInt(stringTokenizer.nextToken());
 
-        downLinks = new ArrayList<>(n + 1);
-        upLinks = new ArrayList<>(n + 1);
+        links = new ArrayList<>(n + 1);
+        entry = new int[n + 1];
+        breakaway = new int[n + 1];
 
         for (int i = 0; i <= n; i++) {
-            downLinks.add(new ArrayList<>());
-            upLinks.add(new ArrayList<>());
+            links.add(new ArrayList<>());
         }
 
         for (int i = 0; i < n - 1; i++) {
@@ -29,31 +30,33 @@ public class Q3 {
             int up = Integer.parseInt(stringTokenizer.nextToken());
             int down = Integer.parseInt(stringTokenizer.nextToken());
 
-            downLinks.get(up).add(down);
-            upLinks.get(down).add(up);
+            links.get(up).add(down);
         }
 
-        List<int[]> questions = new ArrayList<>(questionCnt);
+        start(1, 0);
 
         for (int i = 0; i < questionCnt; i++) {
             stringTokenizer = new StringTokenizer(br.readLine());
             int up = Integer.parseInt(stringTokenizer.nextToken());
             int down = Integer.parseInt(stringTokenizer.nextToken());
 
-            questions.add(new int[]{up, down});
-        }
-
-        for (int[] question : questions) {
-            if (isRight(question[0], question[1])) {
-                System.out.println("yes");
+            if (entry[up] < entry[down] && breakaway[up] > breakaway[down]) {
+                System.out.println("YES");
             } else {
-                System.out.println("no");
+                System.out.println("NO");
             }
         }
     }
 
+    private static int start(int currentNode, int index) {
+        entry[currentNode] = ++index;
 
-    private static boolean isRight(int up, int down) {
-        return false;
+        for (Integer next : links.get(currentNode)) {
+            index = start(next, index);
+        }
+
+        breakaway[currentNode] = ++index;
+
+        return index;
     }
 }
