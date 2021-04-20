8d701008c2abde570bc7a804d9636723ede94a54
taehyun <taehyun16@naver.com>
2021-04-20 14:22:26 +0900

    월간 코드 챌린지2 - 음양 더하기

---
+++ src/programmers/montyly_code_challenge2/음양Plus.java
@@ -0,0 +1,17 @@
+package programmers.montyly_code_challenge2;
+
+public class 음양Plus {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+
+        return answer;
+    }
+}
