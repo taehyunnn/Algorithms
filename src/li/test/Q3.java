6d3b14e64ba86a6ca39b336dff24964cf90a487e
taehyun <taehyun16@naver.com>
2021-05-03 15:18:46 +0900

    li_test - 입실, 퇴실 만난 수 체크
    while문으로 퇴실 가능한 사람 모두 제거
    
    입실 1명 추가
    
    반복

--- src/li/test/Q3.java
+++ src/li/test/Q3.java
@@ -1,36 +1,49 @@
 package li.test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Q3 {
-    public int[] solution(int[] enter, int[] leave) {
-        int[] answer = new int[enter.length+1];
 
-        List<Integer> room = new ArrayList<>();
+    private Set<Integer> room = new HashSet<>();
+
+    public int[] solution(int[] enter, int[] leave) {
+        int[] answer = new int[enter.length + 1];
 
         int enterIndex = 0;
         int leaveIndex = 0;
 
-        while (enterIndex != enter.length || leaveIndex != leave.length) {
-            // 방에 있던 사람 중에 1명이 떠나기 전까지 방에 계속 추가
-            do {
-                room.add(enter[enterIndex++]);
-            } while (enterIndex+1 < enter.length && enter[enterIndex] != leave[leaveIndex]);
-
-            // 한 명이 떠나면 떠날 때 방 안에 있던 사람들 모두 카운트 증가
-            leaveIndex++;
-            for (int i = enterIndex-1; i >= 0; i--) {
-                answer[room.get(i)]++;
+        while (leaveIndex != leave.length) {
+            // 다음 차례로 나갈 사람이 방에 있으면 나간다.
+            while (leaveIndex != leave.length && room.contains(leave[leaveIndex])) {
+                room.remove(leave[leaveIndex]);
+                leaveIndex++;
             }
-            // 한 명 떠나보냄
-            room.remove(enter[enterIndex - 1]);
+
+            if (enterIndex == enter.length) {
+                continue;
+            }
+
+            // 1명 추가
+            int nextEnterNum = enter[enterIndex];
+            for (Integer next : room) {
+                answer[next]++;
+                answer[nextEnterNum]++;
+            }
+            room.add(nextEnterNum);
+            enterIndex++;
         }
+
+        for (int i = 1; i < answer.length; i++) {
+            int num = answer[i];
+            System.out.print(num + " ");
+        }
+        System.out.println();
 
         return answer;
     }
 
     public static void main(String[] args) {
         new Q3().solution(new int[]{1, 3, 2}, new int[]{1, 2, 3});
+        new Q3().solution(new int[]{1, 4, 2, 3}, new int[]{ 2,1,3,4});
     }
 }
